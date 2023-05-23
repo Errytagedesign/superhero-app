428da719294319a21248cee27034e5555fe98a3b
RyanWalck <rwalck33@gmail.com>
2023-05-23 08:13:32 -0400

    App.js updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Nav from './components/Nav';
+import Home from './pages/Home';
+
 
 function App() {
     return (
         <div className="App">
+            <Nav />
             <h1>Superhero App</h1>
-
+            <Home />
         </div>
     );
   
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,6 @@
 function Home() {
     return(
-        <>
-            <h2>This is my Homepage</h2>
-            <p>Welcome to my Superhero App</p>
-        </>
+            <h2>Homepage</h2>
     )
 }
 
